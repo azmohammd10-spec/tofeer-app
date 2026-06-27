312b27a174243ef5f3dcb87889ad39e91b2de514
mohammd Bara <azmohammd10@gmail.com>
2026-06-27 15:26:59 +0300

    ربط فيرباص

--- js/firebase.js
+++ js/firebase.js
@@ -3,20 +3,22 @@ import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
-// إعدادات مشروعك في Firebase
-const firebaseConfig = {
-  apiKey: "PUT_YOUR_API_KEY_HERE",
-  authDomain: "star-social-bdfb2.firebaseapp.com",
-  projectId: "star-social-bdfb2",
-  storageBucket: "star-social-bdfb2.firebasestorage.app",
-  messagingSenderId: "256449419301",
-  appId: "1:256449419301:web:970b4905d6beb86c4087cc"
+// إعدادات مشروعك
+ const firebaseConfig = { 
+  apiKey : "AIzaSyCEuOcQIvg-chEXfwHM8JAxFeYGpaOIoP4" , 
+  authDomain : "star-social-bdfb2.firebaseapp.com" , 
+  معرّف المشروع : "star-social-bdfb2" ، 
+  storageBucket : "star-social-bdfb2.firebasestorage.app" , 
+  messagingSenderId : "256449419301" , 
+  معرف التطبيق : "1:256449419301:web:970b4905d6beb86c4087cc " 
+  معرف القياس : "G-JYT3F33K7Y" 
 };
+ 
 
 // تشغيل Firebase
 const app = initializeApp(firebaseConfig);
 
-// الخدمات الأساسية
+// الخدمات
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
