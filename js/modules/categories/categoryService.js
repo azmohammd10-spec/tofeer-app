4afe4f4bd5e28102611920e5efcdbb4a498b27bc
mohammd Bara <azmohammd10@gmail.com>
2026-07-06 09:16:28 +0300

    Refactor category service to load and retrieve categories
    Refactored category service to load categories from JSON and store them in a local variable. Added error handling for the loading process.

--- js/modules/categories/categoryService.js
+++ js/modules/categories/categoryService.js
@@ -1,4 +1,42 @@
-export async function getCategories() {
-  const res = await fetch("../data/categories.json");
-  return await res.json();
+// =========================
+// Category Service
+// =========================
+
+let categories = [];
+
+// تحميل التصنيفات
+export async function loadCategories() {
+
+    try {
+
+        const response = await fetch("../../data/categories.json");
+
+        categories = await response.json();
+
+        return categories;
+
+    } catch (error) {
+
+        console.error("Failed to load categories:", error);
+
+        return [];
+
+    }
+
+}
+
+// جميع التصنيفات
+export function getCategories() {
+
+    return categories;
+
+}
+
+// جلب تصنيف واحد
+export function getCategory(id) {
+
+    return categories.find(category =>
+        category.id === id
+    );
+
 }
