9e6889f45fd2492ca6903714b7d5bd9bb0275656
mohammd Bara <azmohammd10@gmail.com>
2026-07-06 13:52:36 +0300

    Update shapeTool.js

--- js/editor/tools/shapeTool.js
+++ js/editor/tools/shapeTool.js
@@ -1,1 +1,58 @@
+// =========================
+// Shape Tool
+// =========================
 
+import { addElement } from "../../modules/design/designService.js";
+
+// إضافة مربع
+export function createRectangle() {
+
+    const element = {
+        id: crypto.randomUUID(),
+        type: "shape",
+        shapeType: "rectangle",
+        x: 100,
+        y: 100,
+        width: 150,
+        height: 150,
+        background: "#3b82f6",
+        borderRadius: 0
+    };
+
+    addElement(element);
+
+    return element;
+
+}
+
+// إضافة دائرة
+export function createCircle() {
+
+    const element = {
+        id: crypto.randomUUID(),
+        type: "shape",
+        shapeType: "circle",
+        x: 120,
+        y: 120,
+        width: 150,
+        height: 150,
+        background: "#ef4444",
+        borderRadius: 999
+    };
+
+    addElement(element);
+
+    return element;
+
+}
+
+// تغيير اللون
+export function setShapeColor(element, color) {
+    element.background = color;
+}
+
+// تغيير الحجم
+export function resizeShape(element, width, height) {
+    element.width = width;
+    element.height = height;
+}
